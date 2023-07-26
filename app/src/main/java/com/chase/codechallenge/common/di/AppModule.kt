8a7f5a480f4b2b21155c8bf85df2bc74a8bf4a9c
akkhigan <ganesh.v.konda@gmail.com>
2023-07-25 21:04:45 -0500

    Adding App Module

---
+++ app/src/main/java/com/chase/codechallenge/common/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.chase.codechallenge.common.di
+
+import android.app.Application
+import com.chase.codechallenge.data.datasource.remote.api.weatherapi.WeatherApi
+import com.chase.codechallenge.common.utils.NetworkService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideWeatherApi() : WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl(NetworkService.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideFusedLocationProviderClient(application: Application): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(application)
+    }
+}
